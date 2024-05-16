dd9a12655f971e9b3eccd1a394af68bd3467bb1c
Alex Stoica <stoica.alex.w98@gmail.com>
2024-05-16 16:42:03 +0300

    refactored main screen card

--- src/main/screens/login-qr/login-qr.js
+++ src/main/screens/login-qr/login-qr.js
@@ -6,12 +6,13 @@ import {
   Settings,
   ArrowRight,
   BurgerMenuWithOutline,
-  Logo,
   LogoWithOutline,
 } from "../../components/icons";
 import { Link } from "react-router-dom";
 import withSocket from "../../hocs/withSocket";
 import qrcode from "qrcode";
+import { pipe } from "ramda";
+import withScreenCard from "../../hocs/withScreenCard";
 
 const LoginQR = ({ socket }) => {
   const [isVideoActive, setIsVideoActive] = useState(false);
@@ -50,75 +51,68 @@ const LoginQR = ({ socket }) => {
   }, [canvasRef, qrToken]);
 
   return (
-    <div className={styles["container"]}>
-      <div className={styles["wrapper--before"]}></div>
-      <div className={styles["logo"]}>
-        <Logo />
-        <div>WHATASPP WEB</div>
-      </div>
-      <div className={styles["container__content"]}>
-        <div className={styles["login-steps-container"]}>
-          <div className={styles["login-steps"]}>
-            <div className={styles["steps__list"]}>
-              <div className={styles["list__title"]}>
-                Use WhatsApp on your computer
-              </div>
-              <ol className={styles["list__content"]}>
-                <li>Open WhatsApp on your phone</li>
-                <li>
-                  Tap <strong>Menu</strong>{" "}
-                  <div className={styles["list__item-icon"]}>
-                    <BurgerMenuWithOutline />
-                  </div>{" "}
-                  on Android, or <strong>Settings</strong>{" "}
-                  <div className={styles["list__item-icon"]}>
-                    <Settings />
-                  </div>{" "}
-                  on iPhone
-                </li>
-                <li>
-                  Tap <strong>Linked devices</strong> and then{" "}
-                  <strong>Link a device</strong>
-                </li>
-                <li>Point your phone at this screen to capture the QR code</li>
-              </ol>
+    <>
+      <div className={styles["login-steps-container"]}>
+        <div className={styles["login-steps"]}>
+          <div className={styles["steps__list"]}>
+            <div className={styles["list__title"]}>
+              Use WhatsApp on your computer
             </div>
-            <div className={styles["qr"]}>
-              <div className={styles["qr__img"]}>
-                <canvas id="qr-canvas" ref={canvasRef} />
-              </div>
-              <div className={styles["qr__logo"]}>
-                <LogoWithOutline />
-              </div>
+            <ol className={styles["list__content"]}>
+              <li>Open WhatsApp on your phone</li>
+              <li>
+                Tap <strong>Menu</strong>{" "}
+                <div className={styles["list__item-icon"]}>
+                  <BurgerMenuWithOutline />
+                </div>{" "}
+                on Android, or <strong>Settings</strong>{" "}
+                <div className={styles["list__item-icon"]}>
+                  <Settings />
+                </div>{" "}
+                on iPhone
+              </li>
+              <li>
+                Tap <strong>Linked devices</strong> and then{" "}
+                <strong>Link a device</strong>
+              </li>
+              <li>Point your phone at this screen to capture the QR code</li>
+            </ol>
+          </div>
+          <div className={styles["qr"]}>
+            <div className={styles["qr__img"]}>
+              <canvas id="qr-canvas" ref={canvasRef} />
+            </div>
+            <div className={styles["qr__logo"]}>
+              <LogoWithOutline />
             </div>
           </div>
         </div>
-        <Link className={styles["link"]} to="#">
-          Link with phone number
+      </div>
+      <Link className={styles["link"]} to="#">
+        Link with phone number
+      </Link>
+      <div className={styles["tutorial"]}>
+        <div className={styles["tutorial__title"]}>Tutorial</div>
+        <Link className={styles["tutorial__link"]} to="#">
+          Need help to get started?
         </Link>
-        <div className={styles["tutorial"]}>
-          <div className={styles["tutorial__title"]}>Tutorial</div>
-          <Link className={styles["tutorial__link"]} to="#">
-            Need help to get started?
-          </Link>
-          <div className={styles["tutorial__media"]}>
-            {!isVideoActive && (
-              <button className={styles["media__button"]} onClick={onVideoPlay}>
-                <ArrowRight />
-              </button>
-            )}
-            <div className={styles["media__img"]}>
-              <video controls ref={videoRef}>
-                <source src={loginTutorialVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-              {!isVideoActive && <img src={loginTutorial} draggable={false} />}
-            </div>
+        <div className={styles["tutorial__media"]}>
+          {!isVideoActive && (
+            <button className={styles["media__button"]} onClick={onVideoPlay}>
+              <ArrowRight />
+            </button>
+          )}
+          <div className={styles["media__img"]}>
+            <video controls ref={videoRef}>
+              <source src={loginTutorialVideo} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            {!isVideoActive && <img src={loginTutorial} draggable={false} />}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default withSocket(LoginQR);
+export default pipe(withScreenCard, withSocket)(LoginQR);
