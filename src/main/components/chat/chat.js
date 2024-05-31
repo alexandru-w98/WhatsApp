bc3da3356f70e1e2aecfa64a0a6bcd42eb58103d
Alex Stoica <stoica.alex.w98@gmail.com>
2024-05-31 18:19:34 +0300

    message component with variations

--- src/main/components/chat/chat.js
+++ src/main/components/chat/chat.js
@@ -2,12 +2,17 @@ import React from "react";
 import ChatNavbar from "../chat-navbar";
 import ChatFooter from "../chat-footer";
 import * as styles from "./chat.css";
+import Message from "../message";
 
 const Chat = () => {
   return (
     <div className={styles["chat"]}>
       <ChatNavbar />
-      <div className={styles["chat__content"]}></div>
+      <div className={styles["chat__content"]}>
+        <Message align="right" />
+        <Message />
+        <Message />
+      </div>
       <ChatFooter />
     </div>
   );
